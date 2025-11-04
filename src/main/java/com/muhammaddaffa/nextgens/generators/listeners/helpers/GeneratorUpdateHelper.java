7a6eb4b09e31bb7fa0a31b81a08c1f7d9ddc11aa
mdaffa48 <muhammaddaffa912@gmail.com>
2025-11-04 07:05:29 +0700

    feat: add more useful methods and silent option

--- src/main/java/com/muhammaddaffa/nextgens/generators/listeners/helpers/GeneratorUpdateHelper.java
+++ src/main/java/com/muhammaddaffa/nextgens/generators/listeners/helpers/GeneratorUpdateHelper.java
@@ -14,19 +14,35 @@ import com.muhammaddaffa.nextgens.utils.VisualAction;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Panda;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public class GeneratorUpdateHelper {
 
-    public static void upgradeGenerator(Player player, ActiveGenerator active, Generator generator, Generator nextGenerator) {
+    public static boolean upgradeGenerator(Player player, ActiveGenerator active) {
+        return upgradeGenerator(player, active, false);
+    }
+
+    public static boolean upgradeGenerator(Player player, ActiveGenerator active, boolean silent) {
+        Generator generator = active.getGenerator();
+        Generator nextGenerator = NextGens.getInstance().getGeneratorManager().getGenerator(generator.nextTier());
+        // Try to upgrade
+        return upgradeGenerator(player, active, generator, nextGenerator, silent);
+    }
+
+    public static boolean upgradeGenerator(Player player, ActiveGenerator active, Generator generator, Generator nextGenerator) {
+        return upgradeGenerator(player, active, generator, nextGenerator, false);
+    }
+
+    public static boolean upgradeGenerator(Player player, ActiveGenerator active, Generator generator, Generator nextGenerator, boolean silent) {
         Block block = active.getLocation().getBlock();
         if (nextGenerator == null) {
             NextGens.DEFAULT_CONFIG.sendMessage(player, "messages.no-upgrade");
             // play bass sound
             Utils.bassSound(player);
-            return;
+            return false;
         }
         // money check
         if (VaultEconomy.getBalance(player) < generator.cost()) {
@@ -36,20 +52,20 @@ public class GeneratorUpdateHelper {
                     .add("{remaining}", Common.digits(VaultEconomy.getBalance(player) - generator.cost())));
             // play bass sound
             Utils.bassSound(player);
-            return;
+            return false;
         }
         // Check requirements
         List<String> requirementsNotPassed = generator.checkRequirements(player, generator.upgradeRequirements());
         if (!requirementsNotPassed.isEmpty()) {
             Common.sendMessage(player, requirementsNotPassed);
             Utils.bassSound(player);
-            return;
+            return false;
         }
         // call the custom events
         GeneratorUpgradeEvent upgradeEvent = new GeneratorUpgradeEvent(generator, player, nextGenerator);
         Bukkit.getPluginManager().callEvent(upgradeEvent);
         if (upgradeEvent.isCancelled()) {
-            return;
+            return false;
         }
         // take the money from player
         VaultEconomy.withdraw(player, generator.cost());
@@ -57,10 +73,12 @@ public class GeneratorUpdateHelper {
         NextGens.getInstance().getGeneratorManager().registerGenerator(player, nextGenerator, block);
         // visual actions
         FileConfiguration config = NextGens.DEFAULT_CONFIG.getConfig();
-        VisualAction.send(player, config, "generator-upgrade-options", new Placeholder()
-                .add("{previous}", generator.displayName())
-                .add("{current}", nextGenerator.displayName())
-                .add("{cost}", Common.digits(generator.cost())));
+        if (!silent) {
+            VisualAction.send(player, config, "generator-upgrade-options", new Placeholder()
+                    .add("{previous}", generator.displayName())
+                    .add("{current}", nextGenerator.displayName())
+                    .add("{cost}", Common.digits(generator.cost())));
+        }
         // play particle
         Executor.async(() -> {
             if (NextGens.DEFAULT_CONFIG.getConfig().getBoolean("generator-upgrade-options.particles")) {
@@ -69,6 +87,7 @@ public class GeneratorUpdateHelper {
         });
         // give cashback to the player
         Utils.performCashback(player, NextGens.getInstance().getUserManager(), generator.cost());
+        return true;
     }
 
 }
