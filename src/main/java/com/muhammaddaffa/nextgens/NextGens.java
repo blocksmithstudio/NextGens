29eade7d0f244bd095a0bbe5f176017b89bf2bac
mdaffa48 <muhammaddaffa912@gmail.com>
2025-11-11 10:38:53 +0700

    feat: make world multipliers config not reset on restart

--- src/main/java/com/muhammaddaffa/nextgens/NextGens.java
+++ src/main/java/com/muhammaddaffa/nextgens/NextGens.java
@@ -309,7 +309,7 @@ public final class NextGens extends JavaPlugin {
         File eventsFile = new File(this.getDataFolder(), "events.yml");
 
         try {
-            ConfigUpdater.update(this, "config.yml", configFile, List.of("sell-options.sound"));
+            ConfigUpdater.update(this, "config.yml", configFile, List.of("sell-options.sound", "world-multipliers"));
             ConfigUpdater.update(this, "events.yml", eventsFile, List.of("events.events"));
         } catch (IOException ex) {
             Logger.severe("Failed to update the config.yml!");
